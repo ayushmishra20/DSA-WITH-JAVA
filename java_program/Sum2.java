14ad9af83d4497d4670db1f8bf9daf8c9f47d36c
Ayush Mishra <officialayushmishra002@gmail.com>
2026-04-28 07:07:07 +0530

    class and methods

---
+++ java_program/Sum2.java
@@ -0,0 +1,48 @@
+package java_program;
+
+import java.util.*;
+
+public class Sum2 {
+    public static void main(String[] args) {
+        // int ans = Sum2;
+
+        // Addition();
+        // greetings();
+        // int result = summition(10, 10);
+        // System.out.println(result);
+        // String message = greet();
+        // System.out.println(message);
+    }
+
+    // Return type int Method
+    static int Addition() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number 1: ");
+        int a = in.nextInt();
+        System.out.println("Enter Second number 2: ");
+        int b = in.nextInt();
+        int add = a + b;
+        System.out.println(add);
+        return add;
+    }
+
+    // Greeting Method
+    static void greetings() {
+        System.out.println("Hello Ayush...");
+    }
+
+    // Retur type int method
+    static int summition(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // Return Type String
+    static String greet(){
+        String greeting = "Hello Ayush..";
+        return greeting;
+    }
+
+
+
+}
