5c49cc44aa12e8b848f95927237de66efd365133
Ayush Mishra <officialayushmishra002@gmail.com>
2026-06-06 22:30:22 +0530

    Switch case with enhanced formate

--- java_program/Switch.java
+++ java_program/Switch.java
@@ -6,15 +6,19 @@ public class Switch {
         Scanner in = new Scanner(System.in);
         Integer day = in.nextInt();
         switch (day){
-            case 1 -> System.out.println("Weekday");
-            case 2 -> System.out.println("Weekday");
-            case 3 -> System.out.println("Weekday");
-            case 4 -> System.out.println("Weekday");
+            case 1, 2, 3, 4 -> System.out.println("Weekday");
             case 5 -> System.out.println("Halfday");
-            case 6 -> System.out.println("Weekend");
-            case 7 -> System.out.println("Weekend");
-
+            case 6, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Invalid day entered");
         }
 
+        String name = in.next();
+        switch (name){
+            case "Ayush" -> System.out.println("The user name is Ayush");
+            case "Harsh" -> System.out.println("The user name is Harsh");
+            default -> System.out.println("Unknown user");
+        }
+        
+        in.close();
     }
 }
