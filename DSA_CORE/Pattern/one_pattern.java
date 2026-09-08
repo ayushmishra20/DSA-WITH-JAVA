e8a04cbcfb575eb9305aadb33a5ded15b61467d2
Ayush Mishra <officialayushmishra002@gmail.com>
2026-09-08 20:55:41 +0530

    Pattern

---
+++ DSA_CORE/Pattern/one_pattern.java
@@ -0,0 +1,13 @@
+package Pattern;
+import java.util.*;
+
+public class one_pattern {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+
+    for(int row = 0; row = n; row++){
+        for(int col = 0; col = n ; col++){
+            System.out.println("*");
+        }
+    }
+}
